d42aeead93a3e62ef9192965e6ba9b7ffe198b8a
Christian Weiss <christian@weiss2.de>
2017-07-06 17:09:39 +0200

    fixes URL parsing in PluBaNavigator

--- src/PluBaNavigator.ts
+++ src/PluBaNavigator.ts
@@ -13,8 +13,25 @@ export class PluBaNavigator {
         return this.window.location.href;
     }
 
+    currentHostname(): string {
+        return new URL(this.currentUrl()).hostname;
+    }
+
     hostHref(): string {
-        return this.currentUrl().substr(0, this.currentUrl().indexOf('\/mars\/'));
+        return this.currentHref();
+    }
+
+    currentHref(): string {
+        const url = new URL(this.currentUrl());
+        let result = '';
+        if (url.protocol) {
+            result += url.protocol + '://';
+        }
+        result += url.host;
+        if (url.port) {
+            result += url.port;
+        }
+        return result;
     }
 
 }
